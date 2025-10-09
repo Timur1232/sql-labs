ab469de6008984ace7780f318502d9a9de84597e
Timur <timtimch123@gmail.com>
2025-10-09 23:35:21 +0300

    lab1 done

--- lab1/tasks.sql
+++ lab1/tasks.sql
@@ -1,3 +1,5 @@
+select '6:' as '';
+-- 6
 select
     o.id as order_id,
     o.total_price as subtotal,
@@ -8,3 +10,116 @@ select
     (o.total_price + ds.price) * 0.9 as final_price_with_discount
 from orders o
 join delivery_service ds on o.delivery_service_id = ds.id;
+
+select '7:' as '';
+-- 7
+select
+    category,
+    count(*) as product_count,
+    avg(price) as avg_price,
+    avg(price * 1.1) as avg_price_with_10p_increase
+from products;
+
+select '8:' as '';
+-- 8
+select 
+    o.status,
+    ds.name as delivery_service,
+    avg(o.total_price) as avg_order_value,
+    sum(o.total_price) as total_revenue
+from orders o
+join delivery_service ds on o.delivery_service_id = ds.id
+group by o.status;
+
+select '9:' as '';
+-- 9
+select 
+    u.name,
+    count(o.id) as order_count,
+    sum(o.total_price) as total_spent
+from users u
+join orders o on u.id = o.user_id
+group by u.id, u.name, u.surname
+having count(o.id) >= 2 and sum(o.total_price) > 100;
+
+select '11:' as '';
+-- 11
+select 
+    u.name,
+    u.address,
+    o.id as order_id,
+    o.total_price,
+    o.status,
+    ds.name as delivery_service
+from users u
+join orders o on u.id = o.user_id
+join delivery_service ds on o.delivery_service_id = ds.id
+where u.access_level >= 2;
+
+select '12:' as '';
+-- 12
+select
+    u.id as user_id,
+    u.name,
+    u.surname,
+    u.email
+from users u
+where not exists (
+    select 1
+    from orders o
+    where not exists (
+        select 1
+        from users ui
+        join orders oi on oi.user_id = ui.id
+        where ui.id = u.id
+        and oi.status = o.status
+    )
+);
+
+select '13:' as '';
+-- 13
+select
+    u.sign_up_date as date
+from users u
+where u.access_level >= 2 and u.email like '%@gmail.com'
+union
+select
+    o.order_date as date
+from orders o
+where o.status = 'доставлен' and o.total_price > 100;
+
+select '14:' as '';
+-- 14
+select
+    u.id as user_id,
+    u.name,
+    u.surname,
+    u.email
+from users u
+where not exists (
+    select 1
+    from products p
+    where p.category = 'одежда'
+    and not exists (
+        select 1
+        from ordered_products op
+        join orders o on op.order_id = o.id
+        where o.user_id = u.id
+        and op.product_id = p.id
+    )
+);
+
+select '15:' as '';
+-- 15
+select
+    p.name as product_name,
+    p.price as product_price,
+    p.category as product_category
+from products p
+where p.id not in (
+    select id
+    from products
+    where rating < 3 or manufacturer = 'Sony'
+);
+
+
--- lab1/tests.sql
+++ lab1/tests.sql
@@ -1,11 +1,11 @@
 .header on
 .mode column
+pragma foreign_keys = on;
 
 -- тест 1
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 1]============' as '';
 select * from users where id in (1, 2);
 select '' as '';
 select * from orders where user_id in (1, 2);
@@ -13,6 +13,7 @@ select '' as '';
 select * from ordered_products where order_id in (select id from orders where user_id in (1, 2));
 
 delete from users where id = 1;
+select '============[после]============' as '';
 
 select '' as '';
 select * from users where id in (1, 2);
@@ -22,15 +23,15 @@ select '' as '';
 select * from ordered_products where order_id in (select id from orders where user_id in (1, 2));
 
 -- тест 2
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 2]============' as '';
 select * from users where id = 2;
 select '' as '';
 select * from orders where user_id = 2;
 
 update users set id = 200 where id = 2;
+select '============[после]============' as '';
 
 select '' as '';
 select * from users where id in (2, 200);
@@ -38,15 +39,15 @@ select '' as '';
 select * from orders where user_id in (2, 200);
 
 -- тест 3
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 3]============' as '';
 select * from delivery_service where id in (1, 2);
 select '' as '';
 select * from orders where delivery_service_id in (1, 2);
 
 delete from delivery_service where id = 1;
+select '============[после]============' as '';
 
 select '' as '';
 select * from delivery_service where id in (1, 2);
@@ -54,15 +55,15 @@ select '' as '';
 select * from orders where delivery_service_id in (1, 2);
 
 -- тест 4
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 4]============' as '';
 select * from delivery_service where id = 2;
 select '' as '';
 select * from orders where delivery_service_id = 2;
 
 update delivery_service set id = 300 where id = 2;
+select '============[после]============' as '';
 
 select '' as '';
 select * from delivery_service where id in (2, 300);
@@ -70,10 +71,9 @@ select '' as '';
 select * from orders where delivery_service_id in (2, 300);
 
 -- тест 5
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 5]============' as '';
 select * from products where id in (4, 5);
 select '' as '';
 select * from product_images where product_id in (4, 5);
@@ -81,6 +81,7 @@ select '' as '';
 select * from ordered_products where product_id in (4, 5);
 
 delete from products where id = 4;
+select '============[после]============' as '';
 
 select '' as '';
 select * from product_images where product_id in (4, 5);
@@ -88,10 +89,9 @@ select '' as '';
 select * from ordered_products where product_id in (4, 5);
 
 -- тест 6
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 6]============' as '';
 select * from products where id = 5;
 select '' as '';
 select * from product_images where product_id = 5;
@@ -99,6 +99,7 @@ select '' as '';
 select * from ordered_products where product_id = 5;
 
 update products set id = 500 where id = 5;
+select '============[после]============' as '';
 
 select '' as '';
 select * from products where id in (5, 500);
@@ -108,15 +109,15 @@ select '' as '';
 select * from ordered_products where product_id in (5, 500);
 
 -- тест 7
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 7]============' as '';
 select * from orders where id in (3, 6);
 select '' as '';
 select * from ordered_products where order_id in (3, 6);
 
 delete from orders where id = 3;
+select '============[после]============' as '';
 
 select '' as '';
 select * from orders where id in (3, 6);
@@ -124,15 +125,15 @@ select '' as '';
 select * from ordered_products where order_id in (3, 6);
 
 -- тест 8
+.read test-data.sql
 
-select '' as '';
-select '========================' as '';
-select '' as '';
+select '============[тест 8]============' as '';
 select * from orders where id = 6;
 select '' as '';
 select * from ordered_products where order_id = 6;
 
 update orders set id = 600 where id = 6;
+select '============[после]============' as '';
 
 select '' as '';
 select * from orders where id in (6, 600);
